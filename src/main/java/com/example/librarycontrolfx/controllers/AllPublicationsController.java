1b201a0ede949eeaa9e3e84506febc4a1a1c0d59
Leonardo <leonardo.appio@gmail.com>
2021-11-26 20:14:41 -0300

    tela de todas as publicações adicionada

---
+++ src/main/java/com/example/librarycontrolfx/controllers/AllPublicationsController.java
@@ -0,0 +1,36 @@
+package com.example.librarycontrolfx.controllers;
+
+import com.example.librarycontrolfx.HelloApplication;
+import com.example.librarycontrolfx.models.Database;
+import com.example.librarycontrolfx.models.Publication;
+import com.example.librarycontrolfx.models.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class AllPublicationsController {
+    @FXML
+    private ListView publicationsList;
+
+    public void returnButton(ActionEvent e) throws IOException {
+        HelloApplication.goToScreen("publication-menu.fxml");
+    }
+    public void initialize() throws SQLException{
+
+        ObservableList<String> publicationListView = FXCollections.observableArrayList ();
+        List<Publication> publications = Database.getAllPublication();
+
+        for(Publication publication : publications){
+            publicationListView.add(publication.id+" - "+publication.title+" - "+publication.author+" - "+publication.quantity);
+        }
+
+        publicationsList.setItems(publicationListView);
+
+    }
+}
